39e76dd80f13c023dec84669da328c5d0cfd7ddb
Harvey McQueen <hmcqueen@gmail.com>
2014-03-03 10:02:02 -0600

    Initial commit

---
+++ src/main/java/edu/acu/cs/spring/security/cas/userdetails/GrantedAuthorityFromMemberOfAssertionAttributeUserDetailsService.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2014 Abilene Christian University.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.acu.cs.spring.security.cas.userdetails;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.InvalidNameException;
+import javax.naming.ldap.LdapName;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jasig.cas.client.validation.Assertion;
+import org.springframework.security.cas.userdetails.AbstractCasAssertionUserDetailsService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Populates the {@link org.springframework.security.core.GrantedAuthority}s for a user by reading a
+ * list of values from a memberOf attribute returned as part of the CAS response. Each value of the
+ * attribute is turned into a GrantedAuthority by creating a new {@link javax.naming.ldap.LdapName}
+ * and getting the last {@link javax.naming.ldap.Rdn} value.
+ * <p>
+ * For example, "CN=SSO_ADMIN,OU=roles,DC=spring,DC=io" would be converted to a new
+ * {@link org.springframework.security.core.GrantedAuthority}("SSO_ADMIN")
+ *
+ * @author Harvey McQueen
+ */
+public class GrantedAuthorityFromMemberOfAssertionAttributeUserDetailsService extends AbstractCasAssertionUserDetailsService {
+
+    private static final Log logger = LogFactory.getLog(GrantedAuthorityFromMemberOfAssertionAttributeUserDetailsService.class);
+
+    private static final String NON_EXISTENT_PASSWORD_VALUE = "NO_PASSWORD";
+    private static final String DEFAULT_ATTRIBUTE = "memberOf";
+
+    private String attribute = DEFAULT_ATTRIBUTE;
+    private boolean convertToUpperCase = true;
+    private boolean convertSpacesToUnderscores = true;
+
+    @Override
+    protected UserDetails loadUserDetails(Assertion assertion) {
+        final List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        final Object value = assertion.getPrincipal().getAttributes().get(attribute);
+
+        if (value != null) {
+            if (value instanceof List) {
+                final List list = (List) value;
+
+                for (final Object o : list) {
+                    convertObjectAndAddGrantedAuthorityToList(o, grantedAuthorities);
+                }
+            } else {
+                convertObjectAndAddGrantedAuthorityToList(value, grantedAuthorities);
+            }
+        }
+
+        return new User(assertion.getPrincipal().getName(), NON_EXISTENT_PASSWORD_VALUE, true, true, true, true, grantedAuthorities);
+    }
+
+    private void convertObjectAndAddGrantedAuthorityToList(final Object o, final List<GrantedAuthority> grantedAuthorities) {
+        if (o instanceof String) {
+            final String memberOfString = (String) o;
+            try {
+                LdapName name = new LdapName(memberOfString);
+                if (name.size() > 0) {
+                    String value = name.getRdn(name.size() - 1).getValue().toString();
+                    if (this.convertToUpperCase) {
+                        value = value.toUpperCase();
+                    }
+                    if (this.convertSpacesToUnderscores) {
+                        value = value.replace(' ', '_');
+                    }
+                    grantedAuthorities.add(new SimpleGrantedAuthority(value));
+                }
+            } catch (InvalidNameException e) {
+                logger.warn("Couldn't convert \"" + memberOfString + "\" to an LdapName", e);
+            }
+        }
+    }
+
+    /**
+     * Sets the attribute value to be retrieved.
+     *
+     * @param attribute
+     */
+    public void setAttribute(final String attribute) {
+        this.attribute = attribute;
+    }
+
+    /**
+     * Enables/Disables conversion of the returned attribute values to uppercase values.
+     *
+     * @param convertToUpperCase true if it should convert, false otherwise. Default true.
+     */
+    public void setConvertToUpperCase(final boolean convertToUpperCase) {
+        this.convertToUpperCase = convertToUpperCase;
+    }
+
+    /**
+     * Enables/Disables conversion of spaces in the returned attribute values to underscores.
+     *
+     * @param convertSpacesToUnderscores true if it should convert, false otherwise. Default true.
+     */
+    public void setConvertSpacesToUnderscores(final boolean convertSpacesToUnderscores) {
+        this.convertSpacesToUnderscores = convertSpacesToUnderscores;
+    }
+
+}
---
+++ src/test/java/edu/acu/cs/spring/security/cas/userdetails/GrantedAuthorityFromMemberOfAssertionAttributeUserDetailsServiceTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2014 Abilene Christian University.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.acu.cs.spring.security.cas.userdetails;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.jasig.cas.client.authentication.AttributePrincipal;
+import org.jasig.cas.client.validation.Assertion;
+import org.junit.Test;
+import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * @author Harvey McQueen
+ */
+public class GrantedAuthorityFromMemberOfAssertionAttributeUserDetailsServiceTest {
+
+    @Test
+    public void correctlyExtractsNamedAttributeFromAssertionAndConvertsThemToAuthorities() {
+        GrantedAuthorityFromMemberOfAssertionAttributeUserDetailsService uds
+                = new GrantedAuthorityFromMemberOfAssertionAttributeUserDetailsService();
+        uds.setConvertToUpperCase(false);
+        uds.setConvertSpacesToUnderscores(false);
+        uds.setAttribute("a");
+        Assertion assertion = mock(Assertion.class);
+        AttributePrincipal principal = mock(AttributePrincipal.class);
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("a", Arrays.asList("CN=role_a1,OU=roles,DC=spring,DC=io", "CN=role_a2,OU=roles,DC=spring,DC=io"));
+        attributes.put("b", "b");
+        attributes.put("c", "c");
+        attributes.put("d", null);
+        attributes.put("someother", "unused");
+        when(assertion.getPrincipal()).thenReturn(principal);
+        when(principal.getAttributes()).thenReturn(attributes);
+        when(principal.getName()).thenReturn("somebody");
+        CasAssertionAuthenticationToken token = new CasAssertionAuthenticationToken(assertion, "ticket");
+        UserDetails user = uds.loadUserDetails(token);
+        Set<String> roles = AuthorityUtils.authorityListToSet(user.getAuthorities());
+        assertTrue(roles.size() == 2);
+        assertTrue(roles.contains("role_a1"));
+        assertTrue(roles.contains("role_a2"));
+    }
+
+    @Test
+    public void correctlyExtractsDefaultNamedAttributeFromAssertionAndConvertsThemToAuthorities() {
+        GrantedAuthorityFromMemberOfAssertionAttributeUserDetailsService uds
+                = new GrantedAuthorityFromMemberOfAssertionAttributeUserDetailsService();
+        Assertion assertion = mock(Assertion.class);
+        AttributePrincipal principal = mock(AttributePrincipal.class);
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("memberOf", Arrays.asList("CN=role a1,ou=other,OU=roles,DC=spring,DC=io", "CN=role_a2,OU=roles,dc=spring,DC=io", null));
+        attributes.put("someother", "unused");
+        when(assertion.getPrincipal()).thenReturn(principal);
+        when(principal.getAttributes()).thenReturn(attributes);
+        when(principal.getName()).thenReturn("somebody");
+        CasAssertionAuthenticationToken token = new CasAssertionAuthenticationToken(assertion, "ticket");
+        UserDetails user = uds.loadUserDetails(token);
+        Set<String> roles = AuthorityUtils.authorityListToSet(user.getAuthorities());
+        assertTrue(roles.size() == 2);
+        assertTrue(roles.contains("ROLE_A1"));
+        assertTrue(roles.contains("ROLE_A2"));
+    }
+}
